28ff82617468a8c0a0dabd2b4206572744c4d7bd
Aryan Soni <web.vault0516@gmail.com>
2025-08-07 23:19:07 +0530

    added new url to cors

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://rhythem-of-heart-app.onrender.com/'],
+    origin: ['http://localhost:3000', 'https://rhythm-of-heart-app.onrender.com/'],
     credentials: true
 }));
 app.use(bodyParser.json());
